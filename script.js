389b7af96cb8ceafcc4c1e0a97576f73634364f0
Tengiz Machavariani <tezimach@Tengizs-MacBook-Pro.local>
2022-07-15 15:39:28 +0400

    wrote script to enter only positive numbers in amount input, enter positive, non-decimal numbers in quantity input, chnage colors when pushing buttons, counting tip and total when either entering amount, quantity and either pushing tip percent buttons or entering cutom tip percent and reseting numbers.

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-    document.body.addEventListener("laod click", event => {
-        if (event.target.nodeName == "BUTTON") {
-          console.log("Clicked", event.target.textContent);
-        }
-      });
-  });
-
